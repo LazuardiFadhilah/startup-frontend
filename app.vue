40b0724050c283a882e09944e5562979b6c37225
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-01-30 22:34:25 +0700

    refactor: update Nuxt.js configuration and add TailwindCSS support; include new SVG assets

--- app.vue
+++ app.vue
@@ -2,5 +2,7 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <!-- <h1 class="text-4xl font-bold mt-10">Welcome to Nuxt TailwindCSS!</h1>
+    <p class="mt-5 text-lg">This is styled with Poppins Google Font.</p> -->
   </div>
 </template>
