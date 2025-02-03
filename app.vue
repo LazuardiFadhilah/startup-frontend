6c4fb63faadeff7a76de6a7504968ed7c0a31d2c
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2025-02-03 23:29:56 +0700

    refactor: simplify app layout by replacing NuxtRouteAnnouncer and NuxtWelcome with NuxtLayout and NuxtPage

--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <!-- <h1 class="text-4xl font-bold mt-10">Welcome to Nuxt TailwindCSS!</h1>
-    <p class="mt-5 text-lg">This is styled with Poppins Google Font.</p> -->
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
